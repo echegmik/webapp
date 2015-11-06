3a95c7ff49d31cb8a9851a4c3323dc1c201554bf
Mikel Echegoyen <mikel.echegoyen@tieto.com>
2015-11-07 00:29:23 +0200

    add angular-resource for REST Apis

--- nodes.js
+++ nodes.js
@@ -1,9 +1,13 @@
-angular.module('nodeApp', [])
+angular.module('nodeApp', ['ngResource'])
   .controller('NodeController', function( $scope ) {
 
     $scope.nodes = [
-      {id:'A', color:'#E56385'},
-      {id:'B', color:"#62B3E5"},
-      {id:'C', color:"#6CC04A"}];
+      {id:'fd00:ff1:ce0b:a5e0:fcc2:3d00:4:a5ee', color:'#E56385'},
+      {id:'fd00:ff1:ce0b:a5e0:4:a30b:1b:3d3d', color:"#62B3E5"},
+      {id:'fd00:ff1:ce0b:a5e0:4:a30b:1b:891c', color:"#6CC04A"}];
+
+    function putColor(id, color) {
+       return alert ("do something with "+ id);
+      }
 
   });
